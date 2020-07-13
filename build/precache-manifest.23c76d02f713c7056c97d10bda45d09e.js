5195a804ebeedf7080059d3ca8a464911a9882e4
DeonteTB231 <65545639+DeonteTB231@users.noreply.github.com>
2020-07-12 21:01:14 -0400

    Add files via upload

---
+++ build/precache-manifest.23c76d02f713c7056c97d10bda45d09e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2121427772ee9b045e7b2f006d2f6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e2aa030300f5891d7c1",
+    "url": "/static/css/main.5d72f099.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e2aa030300f5891d7c1",
+    "url": "/static/js/main.b6b8c460.chunk.js"
+  },
+  {
+    "revision": "0adeb7b1d8f47cd9bf9f",
+    "url": "/static/js/runtime-main.71617d02.js"
+  },
+  {
+    "revision": "9cbde9eebc82ddc541ea2845e232183b",
+    "url": "/static/media/back.9cbde9ee.jpg"
+  },
+  {
+    "revision": "1fea9439a15b774380921e4ddf2923b7",
+    "url": "/static/media/me.1fea9439.jpg"
+  }
+]);
---
+++ build/precache-manifest.58041eefc4257fdbb2cf9e77b0d171da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8cfb2eb2cf4af8ffc4a32e7dc00d6f",
+    "url": "/dburnett/WEB215/Lesson14/index.html"
+  },
+  {
+    "revision": "e4882c4327a1d9009034",
+    "url": "/dburnett/WEB215/Lesson14/static/css/main.97593b69.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/dburnett/WEB215/Lesson14/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/dburnett/WEB215/Lesson14/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4882c4327a1d9009034",
+    "url": "/dburnett/WEB215/Lesson14/static/js/main.335ccf5a.chunk.js"
+  },
+  {
+    "revision": "58411993be25c8cb6dfa",
+    "url": "/dburnett/WEB215/Lesson14/static/js/runtime-main.e7e47adc.js"
+  },
+  {
+    "revision": "9cbde9eebc82ddc541ea2845e232183b",
+    "url": "/dburnett/WEB215/Lesson14/static/media/back.9cbde9ee.jpg"
+  },
+  {
+    "revision": "1fea9439a15b774380921e4ddf2923b7",
+    "url": "/dburnett/WEB215/Lesson14/static/media/me.1fea9439.jpg"
+  }
+]);
